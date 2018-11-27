842458c3ff828742f5422f3884eb56d0509dc686
Hari Narayanan <smgdark@gmail.com>
2018-11-27 15:54:15 +0530

    full width file manager

--- dashboard/ctrls/fileManager.js
+++ dashboard/ctrls/fileManager.js
@@ -1,5 +1,5 @@
 angular.module('optimusApp')
-    .controller('fileManagerCtrl', function ($rootScope, $scope, $http, $stateParams, $state, $sce) {
+    .controller('fileManagerCtrl', function ($rootScope, $scope, $http, $stateParams, $state, $sce, $location, $interval) {
         $rootScope.checkAuth();
         $rootScope.profile = true;
         $scope.containerId = $stateParams.containerId;
@@ -16,6 +16,7 @@ angular.module('optimusApp')
                     .then(function (res) {
                         if (res.data.status == true) {
                             $scope.appData = res.data.data;
+                            // $scope.getStats();
                             $scope.url = $sce.trustAsResourceUrl(`https://sftp.optimuscp.io/?containerId=${$scope.containerId}&authKey=${$rootScope.authKey}`);
                         } else {
                             $rootScope.toast('Failed', res.data.msg, 'error');
@@ -28,5 +29,27 @@ angular.module('optimusApp')
                 $state.go('dashboard.home');
             }
         };
+        $scope.getStats = () => {
+            $http({
+                    method: 'GET',
+                    url: $rootScope.apiUrl + 'containers/stats',
+                    params: {
+                        authKey: $rootScope.authKey,
+                        containerId: $scope.containerId,
+                    },
+                    ignoreLoadingBar: true
+                })
+                .then((res) => {
+                    if (res.data.status == true) $scope.appData.stats = res.data.data.stats;
+                    else delete $scope.appData.stats;
+                }, () => {
+                    delete $scope.appData.stats;
+                    $rootScope.toast('Failed', 'Unable to establish network connection.', 'error');
+                });
+        };
+        // $interval(() => {
+        //     if ($location.path().includes('/fileManager'))
+        //         $scope.getStats();
+        // }, 60000);
         $scope.getAppInfo();
     });
